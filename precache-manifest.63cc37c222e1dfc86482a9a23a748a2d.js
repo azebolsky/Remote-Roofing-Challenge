564d34ce755bd094407af638c792df85c9dc87b1
ADAM <zebolsky.adam@gmail.com>
2020-07-03 10:43:22 -0500

    Updates

---
+++ precache-manifest.63cc37c222e1dfc86482a9a23a748a2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a829f4b20a29313d98bd1c1f8fcc1492",
+    "url": "/Remote-Roofing-Challenge/index.html"
+  },
+  {
+    "revision": "528f1db3c1bf2ab60e82",
+    "url": "/Remote-Roofing-Challenge/static/css/main.445ae69b.chunk.css"
+  },
+  {
+    "revision": "accb51f9430a116d7822",
+    "url": "/Remote-Roofing-Challenge/static/js/2.d66d9d08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Remote-Roofing-Challenge/static/js/2.d66d9d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "528f1db3c1bf2ab60e82",
+    "url": "/Remote-Roofing-Challenge/static/js/main.25bb3e96.chunk.js"
+  },
+  {
+    "revision": "25d72d1eee57cd752755",
+    "url": "/Remote-Roofing-Challenge/static/js/runtime-main.3eb4e925.js"
+  }
+]);
